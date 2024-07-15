9981fe68db32b8dc72cf3ea8bf93fb7c70d8427d
Xudong ZHAO <xzhao@henix.fr>
2024-07-15 21:22:36 +0200

    Fix Cloudflare pages pipeline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faBlog, faBook, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
